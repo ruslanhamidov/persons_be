592b2f85301bcd15419ebc6c2fb814478fb0748f
rhamidov-f5 <r.hamidov@f-5.az>
2026-07-20 17:30:31 +0400

    initial

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+app.use(express.json())
+
+app.use(morgan(':method :url :status - :response-time ms :date[web]'))
+
+const dateNow = () => {
+  return new Date().toString()
+}
+
+const generateId = () => {
+  return Math.floor(Math.random() * 5000)
+}
+
+const checkName = (name) => {
+  return persons.find(person => person.name === name)
+}
+
+let persons = [
+    {
+      "id": "1",
+      "name": "Arto Hellas",
+      "number": "040-123456"
+    },
+    {
+      "id": "2",
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523"
+    },
+    {
+      "id": "3",
+      "name": "Dan Abramov",
+      "number": "12-43-234345"
+    },
+    {
+      "id": "4",
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+
+app.get('/info', (request, response) => {
+  const count = persons.length
+  const htmlResponse = `
+    <p>Phonebook has info for ${count} people</p>
+    ${dateNow()}
+    `
+  response.send(htmlResponse)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+
+  const resPerson = persons.find(person => person.id === id)
+
+  if (resPerson) {
+    return response.json(resPerson)
+  }
+  return response.status(404).json({"error": "not found"})
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+
+  persons = persons.filter(person => person.id != id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', morgan(':method :url :status :body - :response-time ms :date[web]'),(request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({"error": "name or number is missing" })
+  }
+
+  if (checkName(body.name)) {
+    return response.status(400).json({"error": "name must be unique"})
+  }
+
+  const person = {
+    "id": String(generateId()),
+    "name": body.name,
+    "number": body.number,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+  morgan.token('body', request => JSON.stringify(request.body))
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
