fa4649c26f1e3d262f47c13a86749aac5ced7be4
rhamidov-f5 <r.hamidov@f-5.az>
2026-07-20 18:01:21 +0400

    uibuild

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 app.use(morgan(':method :url :status - :response-time ms :date[web]'))
 
