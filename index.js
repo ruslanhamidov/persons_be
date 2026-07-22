3a8167d7958ef6cbef772f7d1c38efc753243975
rhamidov-f5 <r.hamidov@f-5.az>
2026-07-22 12:09:33 +0400

    uibuild

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/api/persons/:id', (request, response) => {
     })
 })
 
-app.post('/api/persons', morgan(':method :url :status :body - :response-time ms :date[web]'), (request, response) => {
+app.post('/api/persons', morgan(':method :url :status :body - :response-time ms :date[web]'), (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -56,10 +56,11 @@ app.post('/api/persons', morgan(':method :url :status :body - :response-time ms 
   })
 
   person
-    .save()
-    .then(savedPerson => {
-      response.json(savedPerson)
-    })
+  .save()
+  .then(savedPerson => {
+    response.json(savedPerson)
+  })
+  .catch(error => next(error))
 
   morgan.token('body', request => JSON.stringify(request.body))
 })
@@ -99,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
--- models/person.js
+++ models/person.js
@@ -12,8 +12,21 @@ mongoose.connect(url, { family: 4 })
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+              return /\d{2,3}-\d{5}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true
+  }
 })
 
 const Person = mongoose.model('Person', personSchema)
